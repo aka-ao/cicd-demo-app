5acde9949bc5bc9dddc7f12441bc07cd7b93397a
akahori-soma <akahori.soma@gmail.com>
2022-09-27 15:13:44 +0900

    update date

--- src/main/java/com/example/akaao/MainController.java
+++ src/main/java/com/example/akaao/MainController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("modelValue", "2021/09/23 08:33");
+        model.addAttribute("modelValue", "2022/09/27 08:33");
         return "hello";
     }
 }
