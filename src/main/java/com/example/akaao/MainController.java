efb12d89b286ecd8c4996db5b2eaf6c55517debd
akahori-soma <akahori.soma@gmail.com>
2022-10-04 14:09:18 +0900

    update date

--- src/main/java/com/example/akaao/MainController.java
+++ src/main/java/com/example/akaao/MainController.java
@@ -9,7 +9,7 @@ public class MainController {
 
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("modelValue", "2022/09/27 08:33");
+        model.addAttribute("modelValue", "2022/10/04 14:08");
         return "hello";
     }
 }
